b36353036f62c502a309d8f00788d38abc4d951a
minwuh081@gmail.com <minwuh081@gmail.com>
2019-11-27 13:58:16 +0100

    StartPage

--- src/QuizGame.java
+++ src/QuizGame.java
@@ -1,21 +1,14 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class QuizGame extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("StartPage.fxml"));
 
         Scene scene = new Scene(root, 300, 275);
 
@@ -24,6 +17,10 @@ public class QuizGame extends Application {
         stage.show();
     }
 
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 
 }
--- src/StartPageController.java
+++ src/StartPageController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class StartPageController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
