129ffaeff2a4639994ed30f0dc742f03e59d31bd
minwuh081@gmail.com <minwuh081@gmail.com>
2019-11-27 13:36:38 +0100

    javaFx initial

--- src/Controller.java
+++ src/Controller.java
@@ -1,1 +1,18 @@
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TO
+    }
+
+}
