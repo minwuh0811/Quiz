197b1e898402592ef4254daf980596f652c209d9
minwuh081@gmail.com <minwuh081@gmail.com>
2019-12-16 01:10:18 +0100

    Load Questions From DataBase

--- src/sample/QuizGame.java
+++ src/sample/QuizGame.java
@@ -1,17 +1,17 @@
 package sample;
 
+import database.DataBaseConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import object.*;
 
-import java.net.URL;
+import java.awt.*;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 
 public class QuizGame extends Application {
@@ -20,25 +20,23 @@ public class QuizGame extends Application {
     }
 
 
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-//       String javaVersion = System.getProperty("java.version");
-//       String javafxVersion = System.getProperty("javafx.version");
-//       Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-//       Scene scene = new Scene(new StackPane(l), 640, 480);
-//       primaryStage.setScene(scene);
-//       primaryStage.show();
-         Choices choices=new Choices("Min","Anara","Karolin","Ann");
-         choices.setRightChoice("Min");
-         Question question=new Question("Who has the time today?", choices);
-         QuestionStorage questions= new QuestionStorage(question);
-         choices=new Choices("Stockholm","Malmo","Göteborg","Örebro");
-         choices.setRightChoice("Stockholm");
-         question=new Question("Which city is the capital of Sweden?", choices);
-         questions.addQuestion(question);
-         Game game=new Game(questions);
-         QuizPage(game);
+        DataBaseConnection dataBaseConnection = new DataBaseConnection();
+        try(ResultSet resultSet=dataBaseConnection.getStatement().executeQuery("SELECT * FROM questions");
+        ) {
+            System.out.println("Connected.");
+            resultSet.last();
+            System.out.println("Number of lines:" + resultSet.getRow());
+            resultSet.first();
+            QuestionStorage questionStorage=LoadDataBase(resultSet);
+            Game game=new Game(questionStorage);
+            QuizPage(game);
 
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
     }
 
     public void  QuizPage(Game game) throws Exception {
@@ -52,7 +50,17 @@ public class QuizGame extends Application {
     }
 
 
+    public QuestionStorage LoadDataBase(ResultSet resultSet)throws Exception {
+        QuestionStorage questions=new QuestionStorage();
+        do {
+            Choices choices=new Choices(resultSet.getString(3),resultSet.getString(4),resultSet.getString(5),resultSet.getString(6));
+            choices.setRightChoice(resultSet.getString(7));
+            Question question=new Question(resultSet.getString(2),choices);
+            questions.addQuestion(question);
 
+        } while(resultSet.next());
 
+        return questions;
+    }
 
 }
