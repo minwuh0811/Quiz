a7be78d24ce16d0a39f1cb1fc85a535a6a2478af
minwuh081@gmail.com <minwuh081@gmail.com>
2019-12-16 01:12:45 +0100

    Add one more questions to the quiz

--- src/QuizDatabase.sql
+++ src/QuizDatabase.sql
@@ -1,14 +1,15 @@
-
+DROP TABLE IF EXISTS `game`;
+DROP TABLE IF EXISTS `questions`;
 
 CREATE TABLE IF NOT EXISTS `game` (
-  `gameId` varchar(20) NOT NULL,
+  `gameid` int(11) NOT NULL AUTO_INCREMENT,
   `score` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   PRIMARY KEY (`gameid`)
 ) ENGINE=Innodb  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 CREATE TABLE IF NOT EXISTS `questions` (
-  `questionId` varchar(11) NOT NULL,
+  `questionid` int(11) NOT NULL AUTO_INCREMENT,
   `question` varchar(500) NOT NULL,
   `choice1` varchar(100) NOT NULL,
   `choice2` varchar(100) NOT NULL,
@@ -18,4 +19,6 @@ CREATE TABLE IF NOT EXISTS `questions` (
   PRIMARY KEY (`questionid`)
 ) ENGINE=Innodb  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
-INSERT INTO `questions` (`questionId`, `question`, `choice1`, `choice2`, `choice3`, `choice4`, `rightchoice`) VALUES (1, 'Which city is the capital of Sweden?', 'Stockholm', 'Malmo', 'Göteborg', 'Örebro', 'Stockholm')
+INSERT INTO `questions` (`questionid`, `question`, `choice1`, `choice2`, `choice3`, `choice4`, `rightchoice`) VALUES
+(1, 'Which city is the capital of Sweden?', 'Stockholm', 'Malmo', 'Göteborg', 'Örebro', 'Stockholm'),
+(2, 'Who is the president of U.S.A?', 'Donald Trump', 'Barack Obama', 'George W. Bush', 'Bill Clinton', 'Donald Trump');
