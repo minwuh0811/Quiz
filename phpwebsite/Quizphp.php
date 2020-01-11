2c39be0b7ebac81e7743703bc3480836e3d5fc85
minwuh081@gmail.com <minwuh081@gmail.com>
2020-01-11 01:30:16 +0100

    include style sheet in php file remove the link tag

--- phpwebsite/Quizphp.php
+++ phpwebsite/Quizphp.php
@@ -1,13 +1,15 @@
 <?php
 include 'DatabaseConnection.php';
 ?>
+<style>
+    <?php include 'Quiz.css';?>
+</style>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="Quiz.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>
         $(document).ready(function() {
